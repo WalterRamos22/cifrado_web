c2a0b50049b70d94cc3975575931aeaca380e717
WalterRamos22 <wramosc4@miumg.edu.gt>
2026-04-02 00:30:02 -0600

    Rename database from sistema_cifrado to cifrado_web
    Agregando script de creacion de base de datos

--- sql/1.CreacionBD.sql
+++ sql/1.CreacionBD.sql
@@ -1,7 +1,7 @@
 -- Crea una nueva base de datos llamada sistema_cifrado
-CREATE DATABASE sistema_cifrado;
+CREATE DATABASE cifrado_web;
 GO
 
 -- Selecciona la base de datos sistema_cifrado
-USE sistema_cifrado;
+USE cifrado_web;
 GO
