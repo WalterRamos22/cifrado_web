ccf4c8995dce26a4a8037cf58113ab04c0e89801
WalterRamos22 <wramosc4@miumg.edu.gt>
2026-04-02 01:14:17 -0600

    Update db.py
    Conexion desde python hacia server

--- source/db.py
+++ source/db.py
@@ -1,1 +1,13 @@
+--Conexion desde python hacia el servidor--
+import pyodbc
 
+def conectar():
+    conexion = pyodbc.connect(
+        "DRIVER={ODBC Driver 18 for SQL Server};"
+        "SERVER=192.168.1.44,1433;"
+        "DATABASE=cifrado_web;"
+        "UID=usuario1;"
+        "PWD=1234;"
+        "TrustServerCertificate=yes;"
+    )
+    return conexion
